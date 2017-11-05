043a7d1e0acd00635903fd305ca6a355e29cb4dd
Justin Yoo <justindulark@gmail.com>
2017-11-04 23:32:50 -0700

    Added comments detailing corecursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -11,10 +11,18 @@ var getElementsByClassName = function(className) {
     if(node.classList && node.classList.contains(className)) {
       hasClassName.push(node);
     }
+    //why does this work? At first you would think that x++
+    //never happens and the function keeps going into the next childnode
+    //but when there is no more childnodes, it pops back up to the function that
+    //called it and x++ resolves, and test gets called on the node.childNodes[x+1],
+    //and goes deeper into the childNodes list until there is no more, pops up
+    //and does it again. This type of recursion is called corecursion...
+    //recursion with no base case!
     for(var x = 0; x < node.childNodes.length; x++) {
       test(node.childNodes[x]);
     }
   }
+  //test whole page
   test(document.body);
   return hasClassName;
 };
