c7372ca8207a0ad98f65815005ff53d4c0f29607
Justin Yoo <justindulark@gmail.com>
2017-11-04 21:47:49 -0700

    Finished code for stringifyJSON
    First implemented code for primitives for simple cases. This also allows the function to act recursively
    for arrays and objects, by calling stringifyJSON on its own elements or properties

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,41 @@
 
 // but you don't so you're going to write it from scratch:
 
+
 var stringifyJSON = function(obj) {
   // your code goes here
+  var arrayValues = [];
+  var objString = [];
+  if (typeof obj === 'string') {
+  	return '"' + obj + '"';
+  }
+  if (typeof obj === 'number' || obj === undefined || obj === null || typeof obj === 'boolean') {
+  	return "" + obj;
+  }
+  if (Array.isArray(obj)) {
+  	if (obj.length < 1) {
+  		return "[" + arrayValues + "]";
+  	} else {
+  	    obj.forEach(function(element){
+  	       arrayValues.push(stringifyJSON(element)) 
+  	    });
+
+  	    return "[" + arrayValues + "]"
+  	}
+  }
+  //usually need to check if !null, but that is already checked further up
+  if (typeof obj === 'object') {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    } else {
+      for (var key in obj) {
+        //only push if property is not function or undefined
+        if (!(typeof obj[key] === 'function' || typeof obj[key] === 'undefined')){
+            objString.push("" + stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+        }
+      }
+      console.log(objString)
+      return '{' + objString + '}'
+    }
+  }
 };
